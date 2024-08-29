25882b5533772e61a807dd1a4f979142885a9413
NixieJul <nixie8821@gmail.com>
2024-08-29 20:27:16 +0300

    redone

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,7 @@
+'use strict';
+
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    totalPrice = quantity * pricePerDroid;
+    let totalPrice = quantity * pricePerDroid;
     return totalPrice > customerCredits
       ? `Insufficient funds!`
       : `You ordered ${quantity} droids worth ${totalPrice} credits!` ;
